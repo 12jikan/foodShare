44660482882a44abd251424d8fc804c30cd41e59
12jikan <jareem.e.hoff@gmail.com>
2019-05-12 01:39:49 -0400

    moving to my other computer again

--- api/index.js
+++ api/index.js
@@ -6,27 +6,34 @@ const bodyParser = require('body-parser');
 
 // stuff for mongoose|mongo
 const creds = require('./creds/mongodb/creds.json')
-const uri = `mongodb+srv://12jikan:${creds.password}@cluster0-nxdlq.mongodb.net/foodShare?retryWrites=true`;
+const dbName = "foodShare";
+const uri = `mongodb+srv://12jikan:${creds.password}@cluster0-nxdlq.mongodb.net/${dbName}?retryWrites=true`;
 
 // code here
 const app = express();
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-mongoose.connect(uri, { useNewUrlParser: true })
+// create Schemas here
+const personSchema = new Schema({
+    _id: ObjectId,
+    firstname: String,
+    lastname: String,
+});
+
+const person = mongoose.model('Person', personSchema);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const userModel = mongoose.model("user", {
-    firstName: String,
-    lastName: String,
-    userName: String,
-});
 
-app.post("/api", async (req, res, next) => {
-    let person = new userModel(req.body);
-    let result = await user.save();
-    res.send(result);
-}).catch(err => console.error(err))
+mongoose.connect(uri, { useNewUrlParser: true });
+
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(3000, () => {
     console.log('listening at port 3000');
